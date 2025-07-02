61fe4d0288db86363d4e8043543d0c8bd7941d62
CHARUTHA <hubgith29@gmail.com>
2025-07-01 18:37:17 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "uFRH0YYC#SYSm2ifw1mJL8W4sTM3xVkDnjhD-yDuCBe6JNNnfWuY", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
 DEV: process.env.DEV || "94779062397",
